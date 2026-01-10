64f9273b9d6a673cc1019326014bb198d962cc42
shacharlevi953-wq <shacharlevi953@gmail.com>
2026-01-10 23:10:54 +0200

    Add dataset for baseline road segmentation

--- train_baseline.py
+++ train_baseline.py
@@ -8,3 +8,27 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from PIL import Image
+
+class RoadSegmentationDataset(Dataset):
+    def __init__(self, images_dir, masks_dir, transform=None):
+        self.images_dir = images_dir
+        self.masks_dir = masks_dir
+        self.image_files = sorted(os.listdir(images_dir))
+        self.mask_files = sorted(os.listdir(masks_dir))
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.images_dir, self.image_files[idx])
+        mask_path = os.path.join(self.masks_dir, self.mask_files[idx])
+
+        image = Image.open(img_path).convert("RGB")
+        mask = Image.open(mask_path).convert("L")
+
+        if self.transform:
+            image = self.transform(image)
+            mask = self.transform(mask)
+
+        return image, mask
